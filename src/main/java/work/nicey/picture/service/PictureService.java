edc7097c8b8268cd8ad0f24079a1b994a45fbe39
Zhaoyu <1983909571@qq.com>
2025-01-18 14:16:00 +0800

    feat(picture/picture): 后端实现 upload picture 图片上传API 2

--- src/main/java/work/nicey/picture/service/PictureService.java
+++ src/main/java/work/nicey/picture/service/PictureService.java
@@ -1,7 +1,13 @@
 package work.nicey.picture.service;
 
+import org.springframework.web.multipart.MultipartFile;
+import work.nicey.picture.model.dto.picture.PictureUploadRequest;
+import work.nicey.picture.model.entity.User;
+import work.nicey.picture.model.vo.PictureVO;
 import work.nicey.picture.model.entity.Picture;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.io.IOException;
 
 /**
 * @author Zhaoyu
@@ -9,5 +15,16 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2025-01-11 16:12:05
 */
 public interface PictureService extends IService<Picture> {
+    /**
+     * 上传图片
+     *
+     * @param multipartFile
+     * @param pictureUploadRequest
+     * @param loginUser
+     * @return
+     */
+    PictureVO uploadPicture(MultipartFile multipartFile,
+                            PictureUploadRequest pictureUploadRequest,
+                            User loginUser) throws IOException;
 
 }
