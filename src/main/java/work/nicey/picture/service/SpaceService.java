0165014d02360f943810407d86388542670caf4d
Zhaoyu <1983909571@qq.com>
2025-02-05 15:04:40 +0800

    feat(picture/space): space 相关基础接口开发:增加，删除，修改，分页获取，根据 id 获取，获取空间级别列表

--- src/main/java/work/nicey/picture/service/SpaceService.java
+++ src/main/java/work/nicey/picture/service/SpaceService.java
@@ -1,7 +1,15 @@
 package work.nicey.picture.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import work.nicey.picture.model.dto.space.SpaceAddRequest;
+import work.nicey.picture.model.dto.space.SpaceQueryRequest;
 import work.nicey.picture.model.entity.Space;
 import com.baomidou.mybatisplus.extension.service.IService;
+import work.nicey.picture.model.entity.User;
+import work.nicey.picture.model.vo.SpaceVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
 * @author Zhaoyu
@@ -10,4 +18,62 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface SpaceService extends IService<Space> {
 
+    /**
+     * 创建空间
+     *
+     * @param spaceAddRequest
+     * @param loginUser
+     * @return
+     */
+    long addSpace(SpaceAddRequest spaceAddRequest, User loginUser);
+
+
+    /**
+     * 校验空间
+     *
+     * @param space
+     * @param add   是否为创建时检验
+     */
+    void validSpace(Space space, boolean add);
+
+    /**
+     * 根据空间级别填充空间对象
+     *
+     * @param space
+     */
+    void fillSpaceBySpaceLevel(Space space);
+
+    /**
+     * 获取空间包装类（单条）
+     *
+     * @param space
+     * @param request
+     * @return
+     */
+    SpaceVO getSpaceVO(Space space, HttpServletRequest request);
+
+    /**
+     * 获取空间包装类（分页）
+     *
+     * @param spacePage
+     * @param request
+     * @return
+     */
+    Page<SpaceVO> getSpaceVOPage(Page<Space> spacePage, HttpServletRequest request);
+
+    /**
+     * 获取查询对象
+     *
+     * @param spaceQueryRequest
+     * @return
+     */
+    QueryWrapper<Space> getQueryWrapper(SpaceQueryRequest spaceQueryRequest);
+
+    /**
+     * 校验空间权限
+     *
+     * @param loginUser
+     * @param space
+     */
+    void checkSpaceAuth(User loginUser, Space space);
 }
